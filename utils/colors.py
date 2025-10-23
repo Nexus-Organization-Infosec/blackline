7de2c9ada20545ad3dd0b248991907218e849ac6
diputs-sudo <diputs-sudo@proton.me>
2025-10-23 17:43:37 -0400

    add colors.py

---
+++ utils/colors.py
@@ -0,0 +1,20 @@
+# utils/colors.py 
+
+def color (text: str, color_name: str = "reset", bold: bool = False) -> str:
+    colors = {
+        "reset": "\033[0m",
+        "red": "\033[31m",
+        "green": "\033[32m",
+        "yellow": "\033[33m",
+        "blue": "\033[34m",
+        "magenta": "\033[35m",
+        "cyan": "\033[36m",
+        "white": "\033[37m",
+        "purple": "\033[0;35m",
+        "dark_purple": "\033[38;5;17m",
+    }
+
+    color_code = colors.get(color_name.lower(), colors["reset"])
+    if bold:
+        color_code = "\033[1m" + color_code
+    return f"{color_code}{text}{colors['reset']}"
