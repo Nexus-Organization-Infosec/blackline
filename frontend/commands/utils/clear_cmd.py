ac6322b14c2578a4a80f766851e35dfb5605de66
diputs-sudo <diputs-sudo@proton.me>
2025-10-23 17:41:55 -0400

    add clear_cmd.py

---
+++ frontend/commands/utils/clear_cmd.py
@@ -0,0 +1,14 @@
+# frontend/commands/utils/clear_cmd.py 
+ 
+import os
+import platform 
+from utils.display import print_error
+
+def handle_clear():
+    try:
+        if os.name == 'nt': 
+            os.system('cls')
+        else:
+            os.system('clear')
+    except Exception:
+        print_error("Failed to clear terminal.")
