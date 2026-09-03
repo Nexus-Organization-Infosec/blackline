36ed7a01c0446d7f9c4c6d57da681b00dc136629
diputs-sudo <diputs-sudo@proton.me>
2026-09-02 23:57:59 -0400

    update __init__.py

--- blackline/core/recon/__init__.py
+++ blackline/core/recon/__init__.py
@@ -2,12 +2,16 @@
 
 from blackline.core.recon.models import InvalidReconTargetError, ReconStep, ReconTarget, normalize_recon_target
 from blackline.core.recon.pipeline import ReconPipeline, build_recon_pipeline
+from blackline.core.recon.evidence import EvidenceClaim, EvidenceGraph, build_evidence_graph
 
 __all__ = [
     "InvalidReconTargetError",
+    "EvidenceClaim",
+    "EvidenceGraph",
     "ReconPipeline",
     "ReconStep",
     "ReconTarget",
     "build_recon_pipeline",
+    "build_evidence_graph",
     "normalize_recon_target",
 ]
